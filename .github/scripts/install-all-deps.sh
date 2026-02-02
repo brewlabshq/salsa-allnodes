e9bf0cd67dcf4e53f03ad0acc0b2b75ea1b2dd15
Sephiroth <sephiroth@abyss.finance>
2026-02-02 14:04:40 +0000

    Release version v3.1.8-allnodes

--- .github/scripts/install-all-deps.sh
+++ .github/scripts/install-all-deps.sh
@@ -6,15 +6,37 @@ here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 os_name="$1"
 
-# shellcheck source=.github/scripts/install-openssl.sh
-source "$here/install-openssl.sh" "$os_name"
-# shellcheck source=.github/scripts/install-proto.sh
-source "$here/install-proto.sh" "$os_name"
-
 case "$os_name" in
-"Windows") ;;
+"Windows")
+  # initialize vcpkg.json. the builtin-baseline is the commit that contains required version.
+  # the commit hash can be found in https://github.com/microsoft/vcpkg with `git log --pretty=format:"%H %s" | grep openssl`
+  # can be verified with:
+  # web:
+  #     https://github.com/microsoft/vcpkg/blob/__COMMIT_HASH__/versions/o-/openssl.json
+  # command:
+  #     git show __COMMIT_HASH__:versions/o-/openssl.json
+  cat > vcpkg.json <<EOL
+{
+  "dependencies": ["openssl"],
+  "overrides": [
+    {
+      "name": "openssl",
+      "version": "3.4.1"
+    }
+  ],
+  "builtin-baseline": "5ee5eee0d3e9c6098b24d263e9099edcdcef6631"
+}
+EOL
+  vcpkg install --triplet x64-windows-static-md
+  rm vcpkg.json
+  export OPENSSL_LIB_DIR="$PWD/vcpkg_installed/x64-windows-static-md/lib"
+  export OPENSSL_INCLUDE_DIR="$PWD/vcpkg_installed/x64-windows-static-md/include"
+
+  choco install protoc
+  export PROTOC='C:\ProgramData\chocolatey\lib\protoc\tools\bin\protoc.exe'
+  ;;
 "macOS")
-  brew install llvm
+  brew install llvm protobuf
   LIBCLANG_PATH="$(brew --prefix llvm)/lib"
   export LIBCLANG_PATH
   ;;
@@ -32,7 +54,7 @@ case "$os_name" in
       perl
   else
     sudo apt update
-    sudo apt install -y libclang-dev
+    sudo apt install -y libclang-dev pkg-config libudev-dev protobuf-compiler
   fi
   ;;
 *)
